d149694654b16f4a8bd29e0e097b32a1544052c3
RomeoQuebec <69280111+RomeoQuebec@users.noreply.github.com>
2022-02-16 15:33:52 -0300

    m9 beretta

---
+++ lua/arc9/common/attachments/m4_mag_drum.lua
@@ -0,0 +1,24 @@
+ATT.PrintName = "50 Round Drummer"
+ATT.CompactName = "50RD"
+ATT.Icon = Material("entities/arc9_att_m16_mag_60.png", "mips smooth")
+ATT.Description = [[sexsexsexsexsexsexsexsexsexsexsexsexsexsexsexsexsexsexsexsexsex]]
+ATT.SortOrder = 60
+
+ATT.Category = "m4_mag"
+
+ATT.ClipSizeOverride = 50
+
+ATT.AimDownSightsTimeMult = 1.15
+ATT.SprintToFireTimeMult = 1.2
+
+ATT.ReloadTimeMult = 1.15
+
+ATT.FreeAimRadiusMult = 1.25
+ATT.SpeedMult = 0.98
+
+ATT.SwayAdd = 0.15
+ATT.SpeedMult = 0.995
+
+ATT.DropMagazineModel = "models/weapons/arc9/droppedmags/m16_mag_60.mdl"
+
+ATT.SupplyLimitMult = 0.5
--- lua/weapons/arc9_csa_m9.lua
+++ lua/weapons/arc9_csa_m9.lua
@@ -5,47 +5,47 @@ SWEP.Base = "arc9_base"
 SWEP.Spawnable = true
 SWEP.Category = "ARC-9"
 
-SWEP.PrintName = "Maverick Carbine"
-SWEP.TrueName = "M4 Carbine"
+SWEP.PrintName = "Elite Pistol"
+SWEP.TrueName = "Beretta M9"
 
 SWEP.CamQCA = 3
 SWEP.CamOffsetAng = Angle(0, 0, 90)
 
-SWEP.Class = "Assault Rifle"
+SWEP.Class = "Pistol"
 SWEP.Trivia = {
-    Manufacturer = "Colt's Manufacturing Company",
-    Calibre = "5.56x45mm NATO",
-    Mechanism = "Gas-Operated Rotating Bolt",
-    Country = "United States of America",
-    Year = "1994"
+    Manufacturer = "Fabbrica d'Armi Pietro Beretta S.p.A.",
+    Calibre = "9x19mm Parabellum",
+    Mechanism = "Striker-fired",
+    Country = "Italy",
+    Year = "1985"
 }
 
 SWEP.Credits = {
     Author = "Arqu",
 }
 
-SWEP.Description = [[The M4 carbine is a 5.56×45mm NATO, air-cooled, gas-operated, magazine-fed, carbine, assault rifle developed in the United States during the 1980s. It is a shortened version of the m4A2 assault rifle.
-
-The M4 is extensively used by the United States Armed Forces, with decisions to largely replace the m4 rifle in United States Army (starting 2010) and United States Marine Corps (USMC) (starting 2016) combat units as the primary infantry weapon and service rifle. The M4 has been adopted by over 60 countries worldwide, and has been described as "one of the defining firearms of the 21st century".]]
+SWEP.Description = [[The Beretta M9, officially the Pistol, Semiautomatic, 9mm, M9, is the designation for the Beretta 92FS semi-automatic Pistol used by the United States Armed Forces. The M9 was adopted by the United States military as their service Pistol in 1985.]]
 
 if !ARC9:UseTrueNames() then
 
 SWEP.Trivia = {
     Manufacturer = "The Classic & Superb Firearms Company",
-    Calibre = "5.56x45mm NATO",
+    Calibre = "9x19mm Parabellum",
     Mechanism = "Gas-Operated Rotating Bolt",
-    Country = "United States of America",
-    Year = "1994"
+    Country = "Italy",
+    Year = "1985"
 }
 
 SWEP.Description = [[Who the fuck reads this anyways.]]
 
 end
 
-SWEP.ViewModel = "models/weapons/arc9/csa/c_maverick.mdl"
-SWEP.WorldModel = "models/weapons/w_rif_m4a1.mdl"
+SWEP.ViewModel = "models/weapons/arc9/csa/c_elite.mdl"
+SWEP.WorldModel = "models/weapons/w_pis_m9.mdl"
 
-SWEP.Slot = 2
+SWEP.Slot = 1
+
+SWEP.DrawCrosshair = true
 
 SWEP.MirrorVMWM = true
 SWEP.WorldModelOffset = {
@@ -54,7 +54,7 @@ SWEP.WorldModelOffset = {
     Scale = 1
 }
 
-SWEP.DefaultBodygroups = "0000000400000000000000"
+SWEP.DefaultBodygroups = "0000000000000000000000"
 
 -------------------------- DAMAGE PROFILE
 
@@ -77,7 +77,7 @@ SWEP.PhysBulletMuzzleVelocity = 25000
 SWEP.Ammo = "smg1" -- What ammo type this gun uses.
 
 SWEP.ChamberSize = 1 -- The amount of rounds this gun can chamber.
-SWEP.ClipSize = 30 -- Self-explanatory.
+SWEP.ClipSize = 12 -- Self-explanatory.
 SWEP.SupplyLimit = 6 -- Amount of magazines of ammo this gun can take from an ARC9 supply crate.
 SWEP.SecondarySupplyLimit = 2 -- Amount of reserve UBGL magazines you can take.
 
@@ -103,18 +103,18 @@ SWEP.RPM = 800
 -- n: n-round burst.
 SWEP.Firemodes = {
     {
-        Mode = -1,
+        Mode = 1,
         -- add other attachment modifiers
     },
     {
-        Mode = 1
+        Mode = 0
     }
 }
 
 SWEP.AutoBurst = false -- Hold fire to keep firing bursts
 SWEP.PostBurstDelay = 0.05
 SWEP.RunAwayBurst = false -- Burst will keep firing until all of the burst has been expended.
-SWEP.NonResetBurst = true -- Annoying behaviour where you have to shoot ALL THREE BULLETS of a burst before it resets. Supposedly realistic for the m4.
+SWEP.NonResetBurst = true -- Annoying behaviour where you have to shoot ALL THREE BULLETS of a burst before it resets. Supposedly realistic for the m9.
 
 -- Use this hook to modify features of a firemode.
 -- SWEP.Hook_P_ModifyFiremode = function(self, firemode) return firemode end
@@ -179,8 +179,8 @@ SWEP.TracerColor = Color(255, 225, 200) -- Color of tracers. Only works if trace
 -------------------------- POSITIONS
 
 SWEP.IronSights = {
-    Pos = Vector(-3.981, -0.802, 1.049),
-    Ang = Angle(0.662, -0.368, 0),
+    Pos = Vector(-5.66, -5.625, 1.889),
+    Ang = Angle(0.277, 0, 0),
     Midpoint = { -- Where the gun should be at the middle of it's irons
         Pos = Vector(0, 15, -4),
         Ang = Angle(0, 0, -45),
@@ -188,10 +188,10 @@ SWEP.IronSights = {
     Magnification = 1.1,
 }
 
-SWEP.SprintAng = Angle(30, -15, 0)
-SWEP.SprintPos = Vector(-1, 5, 0)
+SWEP.SprintAng = Angle(-0.038, 47.219, 0)
+SWEP.SprintPos = Vector(0.597, 0, -12.742)
 
-SWEP.ActivePos = Vector(0, 5, 0)
+SWEP.ActivePos = Vector(0, 2, 0)
 SWEP.ActiveAng = Angle(0, 0, 0)
 
 -------------------------- HoldTypes
@@ -201,7 +201,7 @@ SWEP.HoldTypeSprint = "passive"
 SWEP.HoldTypeHolstered = "passive"
 SWEP.HoldTypeSights = "ar2"
 SWEP.HoldTypeCustomize = "slam"
-SWEP.HoldTypeBlindfire = "pistol"
+SWEP.HoldTypeBlindfire = "Pistol"
 
 SWEP.AnimShoot = ACT_HL2MP_GESTURE_RANGE_ATTACK_AR2
 SWEP.AnimReload = ACT_HL2MP_GESTURE_RELOAD_AR2
@@ -209,36 +209,38 @@ SWEP.AnimDraw = ACT_HL2MP_GESTURE_RANGE_ATTACK_KNIFE
 
 -------------------------- EFFECTS
 
-SWEP.MuzzleParticle = "muzzleflash_3" -- Used for some muzzle effects.
+SWEP.MuzzleParticle = "muzzleflash_pistol" -- Used for some muzzle effects.
 
-SWEP.ShellModel = "models/shells/shell_556.mdl"
+SWEP.ShellModel = "models/shells/shell_9mm.mdl"
 SWEP.ShellCorrectAng = Angle(0, 180, 0)
-SWEP.ShellScale = 1
+SWEP.ShellScale = 1.5
 SWEP.ShellPhysBox = Vector(0.5, 0.5, 2)
 
 -------------------------- SOUNDS
 
-local path = ")^weapons/m4a1/"
-SWEP.FirstShootSound = path .. "m4a1_unsil-1.wav"
-SWEP.ShootSound = path .. "m4a1_unsil-1.wav"
+local path = ")^weapons/elite/"
+SWEP.FirstShootSound = path .. "elite-1.wav"
+SWEP.ShootSound = path .. "elite_unsil-1.wav"
 SWEP.DistantShootSound = path .. "dist.ogg"
-SWEP.ShootSoundSilencedFirst = path .. "m4a1-1.wav"
-SWEP.ShootSoundSilenced = path .. "m4a1-1.wav"
-SWEP.DryFireSound = "weapons/clipempty_rifle.wav"
+SWEP.ShootSoundSilencedFirst = "weapons/usp/usp1.wav"
+SWEP.ShootSoundSilenced = "weapons/usp/usp1.wav"
+SWEP.DryFireSound = "weapons/clipempty_pistol.wav"
 
 SWEP.FiremodeSound = "arc9/firemode.wav"
 
+/*
  SWEP.Hook_TranslateAnimation = function(swep, anim)
     local elements = swep:GetElements()
 
-    if elements["m4_mag_drum"] then
+    if elements["m9_mag_drum"] then
         return anim .. "_drum"
-    elseif elements["m4_mag_tape"] then
+    elseif elements["m9_mag_tape"] then
         return anim .. "_tape"
-    elseif elements["m4_mag_belt"] then
+    elseif elements["m9_mag_belt"] then
         return anim .. "_belt"
     end
 end 
+*/
 
 SWEP.Animations = {
     ["idle"] = {
@@ -247,7 +249,8 @@ SWEP.Animations = {
     ["draw"] = {
         Source = "deploy",
         EventTable = {
-            {s = path .. "m4a1_deploy.wav", t = 0.0},
+            {s = "weapons/glock/glock_slideback.wav", t = 0.0},
+            {s = path .. "elite_sliderelease.wav", t = 0.5},
         },
     },
     
@@ -266,10 +269,10 @@ SWEP.Animations = {
     ["reload"] = {
         Source = "reload",
         TPAnim = ACT_HL2MP_GESTURE_RELOAD_AR2,
-        Time = 1.5,
+        Time = 1.75,
         EventTable = {
-            {s = path .. "m4a1_clipout.wav", t = 0.0},
-            {s = path .. "m4a1_clipin.wav", t = 0.3},
+            {s = path .. "elite_clipout.wav", t = 0.0},
+            {s = path .. "elite_leftclipin.wav", t = 0.75},
         },
         MinProgress = 1.5,
         LastClip1OutTime = 0.9,
@@ -301,80 +304,9 @@ SWEP.Animations = {
         TPAnim = ACT_HL2MP_GESTURE_RELOAD_AR2,
         Time = 2,
         EventTable = {
-            {s = path .. "m4a1_clipout.wav", t = 0.0},
-            {s = path .. "m4a1_clipin.wav", t = 0.45},
-            {s = path .. "m4a1_boltpull.wav", t = 1.1},
-        },
-        MinProgress = 2,
-        LastClip1OutTime = 0.7,
-        IKTimeLine = {
-            {
-                t = 0,
-                lhik = 1,
-                rhik = 1
-            },
-            {
-                t = 0.2,
-                lhik = 0,
-                rhik = 0
-            },
-            {
-                t = 0.75,
-                lhik = 0,
-                rhik = 0
-            },
-            {
-                t = 1,
-                lhik = 1,
-                rhik = 1
-            },
-        },
-        
-    },
-
-    ---Drum mag reload---
-
-    ["reload_drum"] = {
-        Source = "reload_drum",
-        TPAnim = ACT_HL2MP_GESTURE_RELOAD_AR2,
-        Time = 2,
-        EventTable = {
-            {s = path .. "m4a1_clipout.wav", t = 0.0},
-            {s = path .. "m4a1_clipin.wav", t = 0.65},
-        },
-        MinProgress = 1.5,
-        LastClip1OutTime = 0.9,
-        IKTimeLine = {
-            {
-                t = 0,
-                lhik = 1,
-                rhik = 1
-            },
-            {
-                t = 0.2,
-                lhik = 0,
-                rhik = 0
-            },
-            {
-                t = 0.75,
-                lhik = 0,
-                rhik = 0
-            },
-            {
-                t = 1,
-                lhik = 1,
-                rhik = 1
-            },
-        },
-    },
-    ["reload_empty_drum"] = {
-        Source = "reload_dry_drum",
-        TPAnim = ACT_HL2MP_GESTURE_RELOAD_AR2,
-        Time = 2.5,
-        EventTable = {
-            {s = path .. "m4a1_clipout.wav", t = 0.0},
-            {s = path .. "m4a1_clipin.wav", t = 0.65},
-            {s = path .. "m4a1_boltpull.wav", t = 1.5},
+            {s = path .. "elite_clipout.wav", t = 0.0},
+            {s = path .. "elite_leftclipin.wav", t = 0.8},
+            {s = path .. "elite_sliderelease.wav", t = 1.5},
         },
         MinProgress = 2,
         LastClip1OutTime = 0.7,
@@ -407,59 +339,59 @@ SWEP.Animations = {
 -------------------------- ATTACHMENTS
 
 SWEP.AttachmentElements = {
-    ["m4_mag_20"] = {
+    ["m9_mag_20"] = {
         Bodygroups = {
             {5, 1}
         }
     },
-    ["m4_mag_drum"] = {
+    ["m9_mag_drum"] = {
         Bodygroups = {
             {5, 2}
         }
     },
-    ["m4_upper_sbsnc"] = {
+    ["m9_upper_sbsnc"] = {
         Bodygroups = {
             {4, 1},
             {2, 1},
             {3, 5}
         },
     },
-    ["m4_upper_a1"] = {
+    ["m9_upper_a1"] = {
         Bodygroups = {
             {3, 1}
         },
     },
-    ["m4_upper_a2"] = {
+    ["m9_upper_a2"] = {
         Bodygroups = {
             {3, 2}
         },
     },
-    ["m4_grip_skeleton"] = {
+    ["m9_grip_skeleton"] = {
         Bodygroups = {
             {7, 1}
         }
     },
-    ["m4_grip_wood"] = {
+    ["m9_grip_wood"] = {
         Bodygroups = {
             {7, 2}
         }
     },
-    ["m4_barrel_a1"] = {
+    ["m9_barrel_a1"] = {
         Bodygroups = {
             {3, 1}
         },
     },
-    ["m4_barrel_wood"] = {
+    ["m9_barrel_wood"] = {
         Bodygroups = {
             {3, 2}
         },
     },
-    ["m4_barrel_lmg"] = {
+    ["m9_barrel_lmg"] = {
         Bodygroups = {
             {3, 3}
         },
     },
-    ["m4_upper_subsonic"] = {
+    ["m9_upper_subsonic"] = {
         Bodygroups = {
             {2, 1},
             {4, 1},
@@ -470,22 +402,22 @@ SWEP.AttachmentElements = {
             {6, 3}
         }
     },
-    ["m4_stock_solid"] = {
+    ["m9_stock_solid"] = {
         Bodygroups = {
             {6, 1}
         }
     },
-    ["m4_stock_wire"] = {
+    ["m9_stock_wire"] = {
         Bodygroups = {
             {6, 4}
         }
     },
-    ["m4_stock_m607"] = {
+    ["m9_stock_m607"] = {
         Bodygroups = {
             {6, 2}
         }
     },
-    ["m4_stock_wood"] = {
+    ["m9_stock_wood"] = {
         Bodygroups = {
             {6, 3}
         }
@@ -495,7 +427,7 @@ SWEP.AttachmentElements = {
 SWEP.Hook_ModifyBodygroups = function(wep, data)
     local vm = wep:GetOwner():GetViewModel()
     if !vm then return end
-    if wep:HasElement("m4lp") then
+    if wep:HasElement("m9lp") then
         vm:SetBodygroup(6, wep:HasElement("shortfs") and 3 or 1)
     end
 end
@@ -505,17 +437,17 @@ SWEP.Attachments = {
         PrintName = "RECEIVER",
         DefaultName = "RAI RC83 Upper",
         DefaultCompactName = "RC83UPPER",
-        DefaultName_TrueName = "Colt m4A2 Upper",
+        DefaultName_TrueName = "Colt m9A2 Upper",
         DefaultCompactName_TrueName = "A2UPPER",
-        Category = "m4_upper",
-        Bone = "pistol",
+        Category = "m9_upper",
+        Bone = "ValveBiped.Base",
         Pos = Vector(0, -3, 1.5),
         Ang = Angle(90, 0, -90),
     },
     {
         PrintName = "MOUNT",
-        Category = "mount_m4ch",
-        Bone = "pistol",
+        Category = "mount_m9ch",
+        Bone = "ValveBiped.Base",
         ExcludeElements = {"flattop"},
         Pos = Vector(0, -5.15, 3.5),
         Ang = Angle(90, 0, -90),
@@ -523,8 +455,8 @@ SWEP.Attachments = {
     {
         PrintName = "TUBE",
         DefaultName = "Buffer Tube",
-        Category = {"m4_stock", "bt_stock"},
-        Bone = "pistol",
+        Category = {"m9_stock", "bt_stock"},
+        Bone = "ValveBiped.Base",
         Pos = Vector(0, -4, -5.5),
         Ang = Angle(90, 0, -90),
     },
@@ -532,10 +464,10 @@ SWEP.Attachments = {
         PrintName = "BARREL",
         DefaultName = "RAI Ribbed 20\" Barrel",
         DefaultCompactName = "20\"RIB",
-        DefaultName_TrueName = "Colt m4A2 20\" Barrel",
+        DefaultName_TrueName = "Colt m9A2 20\" Barrel",
         DefaultCompactName_TrueName = "20\"A2",
-        Category = {"m4_barrel"},
-        Bone = "pistol",
+        Category = {"m9_barrel"},
+        Bone = "ValveBiped.Base",
         Pos = Vector(0, -3.5, 15),
         Ang = Angle(90, 0, -90),
     },
@@ -543,15 +475,15 @@ SWEP.Attachments = {
         PrintName = "MUZZLE",
         DefaultName = "Factory Flash Hider",
         Category = "muzzle",
-        Bone = "pistol",
+        Bone = "ValveBiped.Base",
         Pos = Vector(0, -3.5, 23),
         Ang = Angle(90, 0, -90),
     },
     {
         PrintName = "MAG",
         DefaultName = "Standard 30 Round",
-        Category = "m4_mag",
-        Bone = "pistol",
+        Category = "m9_mag",
+        Bone = "ValveBiped.Base",
         Pos = Vector(0, 0, 6),
         Ang = Angle(90, 0, -90),
     },
@@ -559,7 +491,7 @@ SWEP.Attachments = {
         PrintName = "AMMO",
         DefaultName = "Standard FMJ",
         Category = {"ammo", "ammo_assault"},
-        Bone = "pistol",
+        Bone = "ValveBiped.Base",
         Pos = Vector(0, 1, 7),
         Ang = Angle(90, 0, -90),
     },
@@ -567,8 +499,8 @@ SWEP.Attachments = {
         PrintName = "GRIP",
         DefaultName = "Factory Grip",
         ExcludeElements = {"gripstock"},
-        Category = "m4_grip",
-        Bone = "pistol",
+        Category = "m9_grip",
+        Bone = "ValveBiped.Base",
         Pos = Vector(0, 0, 1.5),
         Ang = Angle(90, 0, -90),
     },
@@ -576,14 +508,14 @@ SWEP.Attachments = {
         PrintName = "MOUNT",
         Category = "mount_barrel",
         ExcludeElements = {"nomount"},
-        Bone = "pistol",
+        Bone = "ValveBiped.Base",
         Pos = Vector(0, -3, 18),
         Ang = Angle(90, 0, -90),
     },
     {
         PrintName = "IRONS",
-        Category = {"m4lp"},
-        Bone = "pistol",
+        Category = {"m9lp"},
+        Bone = "ValveBiped.Base",
         ExcludeElements = {"fpw"},
         Pos = Vector(0, -6, 18),
         Ang = Angle(90, 0, -90),
